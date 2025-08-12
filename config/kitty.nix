4c9bf9e7d575a718a266e4a129ea79b888e1ec3a
esahin90 <emre.tr@outlook.com>
2025-08-12 09:45:08 +0200

    Change kitty to infinity scrollback

--- config/kitty.nix
+++ config/kitty.nix
@@ -18,7 +18,7 @@ in
       wheel_scroll_min_lines = 1;
       window_padding_width = 4;
       confirm_os_window_close = 0;
-      scrollback_lines = 10000;
+      scrollback_lines = -1;
       enable_audio_bell = false;
       mouse_hide_wait = 60;
       cursor_trail = 1;
